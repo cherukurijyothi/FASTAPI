b8277f1b0ed144f4c1ceb870bff4a7bd6ed837e1
Shashank5555 <saishashank15@gmail.com>
2026-02-05 15:56:57 -0500

    test

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello everyone. This is Shashank! How are you doing? I'm good. Thank you for asking."}
+   ekajfnaskdjnf
 
 @app.get("/health")
 def health():
